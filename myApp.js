495da36de0ad194e3bc503cbb01b6f6ddeaf05b5
saipraveen <ossp.ctr@gmail.com>
2023-10-24 07:16:32 +0000

    feat: practice node and express
    feat: practice node and express

--- myApp.js
+++ myApp.js
@@ -1,39 +1,27 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+console.log("Hello World");
+
+// app.get("/", (req, res) => { res.send("Hello Express")});
+
+// app.get("/", (req, res) => { res.sendFile(__dirname + "/views/index.html")});
+// app.use("/public", express.static(__dirname+"/public"));
+
+// let helloMessage = "Hello json";
+// console.log(process.env.MESSAGE_STYLE);
+// // helloMessage += "-" + process.env.MESSAGE_STYLE;
+// const getHelloMessage = () => (process.env.MESSAGE_STYLE === "uppercase") ? helloMessage.toUpperCase() : helloMessage;
+// const jsonResponse = { "message": getHelloMessage() };
+// app.get("/json", (req, res) => { res.json(jsonResponse) });
+
+app.get("/json", (req, res) => {
+  const messageStyle = process.env.MESSAGE_STYLE;
+  const message = (messageStyle === "uppercase") ? "HELLO JSON" : "Hello json";
+  res.json({"message" : message});
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
