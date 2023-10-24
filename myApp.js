e437b7ea28df486695e77c44117b3282857e6136
saipraveen <ossp.ctr@gmail.com>
2023-10-24 08:43:38 +0000

    feat: practice node and express

--- myApp.js
+++ myApp.js
@@ -2,22 +2,20 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 console.log("Hello World");
+app.use((req, res, next) => {
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+});
 
-// app.get("/", (req, res) => { res.send("Hello Express")});
+app.get("/", (req, res) => { res.send("Hello Express")});
 
 // app.get("/", (req, res) => { res.sendFile(__dirname + "/views/index.html")});
 // app.use("/public", express.static(__dirname+"/public"));
 
-// let helloMessage = "Hello json";
-// console.log(process.env.MESSAGE_STYLE);
-// // helloMessage += "-" + process.env.MESSAGE_STYLE;
-// const getHelloMessage = () => (process.env.MESSAGE_STYLE === "uppercase") ? helloMessage.toUpperCase() : helloMessage;
-// const jsonResponse = { "message": getHelloMessage() };
-// app.get("/json", (req, res) => { res.json(jsonResponse) });
-
 app.get("/json", (req, res) => {
   const messageStyle = process.env.MESSAGE_STYLE;
-  const message = (messageStyle === "uppercase") ? "HELLO JSON" : "Hello json";
+  const helloMessage = "Hello json";
+  const message = (messageStyle === "uppercase") ? helloMessage.toUpperCase() : helloMessage;
   res.json({"message" : message});
 });
 
